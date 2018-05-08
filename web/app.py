939d252ccfd64ac58ce0460be168ef0c7b63be94
Álvaro Beleño <alvarobrod@outlook.es>
2018-05-09 01:02:45 +0200

    Ficheros modificados

--- web/app.py
+++ web/app.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def inicio():
-    return render_template('index.html')
+	return render_template('index.html')
+
+@app.route('/busqueda')
+def busqueda():
+	return render_template('busqueda.html')
 
 app.run('0.0.0.0', 8080, debug = True)
