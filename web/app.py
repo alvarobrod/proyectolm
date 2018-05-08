d8edb7b23488fb66c0da9dc680df9e37234ed9cd
Álvaro Beleño <alvarobrod@outlook.es>
2018-05-08 22:50:07 +0200

    Modificados app.py e index.html

--- web/app.py
+++ web/app.py
@@ -1,4 +1,4 @@
-from Flask import Flask
+from flask import Flask, render_template
 import json
 
 app = Flask(__name__)
@@ -6,3 +6,5 @@ app = Flask(__name__)
 @app.route('/')
 def inicio():
     return render_template('index.html')
+
+app.run('0.0.0.0', 8080, debug = True)
