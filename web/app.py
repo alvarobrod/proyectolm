e7abb72ed179564bbb9f5eccf7ba6a1f75326f5c
Álvaro Beleño <alvarobrod@outlook.es>
2018-05-09 22:26:29 +0200

    Modificaciones en app.py y busqueda.html

--- web/app.py
+++ web/app.py
@@ -16,14 +16,14 @@ def inicio():
 @app.route('/busqueda', methods = ['GET', 'POST'])
 def busqueda():
 	if request.method == 'GET':
-		return render_template('busqueda.html')
+		return render_template('busqueda.html', datos = None)
 	else:
 		titulo_form = request.form['titulo']
 		payload = {'api_key': tmdb_key, 'language': 'es-ES', 'query': titulo_form}
 		r = requests.get(URL_BASE_TMDB + 'movie', params = payload)
 		if r.status_code == 200:
 			js = r.json()
-			titulo = js['results'][0]['title']
-		return render_template('busqueda.html', titulo = titulo)
+			dic_res = {'titulo': js['results'][0]['title'], 'fecha': js['results'][0]['release_date']}
+		return render_template('busqueda.html', datos = dic_res)
 
 app.run('0.0.0.0', 8080, debug = True)
