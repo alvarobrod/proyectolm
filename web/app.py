fd8936d14e8302bea543cf96a80a6efde5635c0e
Álvaro Beleño <alvarobrod@outlook.es>
2018-05-09 20:07:30 +0200

    Modificados app.py y busqueda.html

--- web/app.py
+++ web/app.py
@@ -1,7 +1,13 @@
 from flask import Flask, render_template
 import json
+import requests
+import os
 
 app = Flask(__name__)
+
+# URL_BASE_TMDB = 
+
+tmdb_key = os.environ['tmdb_key']
 
 @app.route('/')
 def inicio():
