e18433f3734c4293a9082d4005a8b78ac2d95ff1
Álvaro Beleño <alvarobrod@outlook.es>
2018-05-09 23:23:40 +0200

    Modificados busqueda.html y funciones.py

--- web/funciones.py
+++ web/funciones.py
@@ -1,2 +1,16 @@
+import requests
+import os
+
+URL_BASE_TMDB = 'https://api.themoviedb.org/3/genre/movie/list'
+
+tmdb_key = os.environ['tmdb_key']
+
 def formathora(hora):
 	return '{}/{}/{}'.format(hora.split('-')[2], hora.split('-')[1], hora.split('-')[0])
+
+def genero(lista):
+	payload = {'api_key': tmdb_key, 'language': 'es-ES'}
+	r = requests.get(URL_BASE_TMDB, params = payload)
+	if r.status_code == 200:
+		js = r.json()
+		generos = js['genres']
