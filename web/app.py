44d792dca250b2cfa500133cb2fb888274ac5953
Álvaro Beleño <alvarobrod@outlook.es>
2018-05-09 21:44:29 +0200

    Modificados app.py y main.css

--- web/app.py
+++ web/app.py
@@ -5,7 +5,7 @@ import os
 
 app = Flask(__name__)
 
-# URL_BASE_TMDB = 
+URL_BASE_TMDB = 'https://api.themoviedb.org/3/search/'
 
 tmdb_key = os.environ['tmdb_key']
 
@@ -18,7 +18,12 @@ def busqueda():
 	if request.method == 'GET':
 		return render_template('busqueda.html')
 	else:
-		titulo = request.form['titulo']
+		titulo_form = request.form['titulo']
+		payload = {'api_key': tmdb_key, 'language': 'es-ES', 'query': titulo_form}
+		r = requests.get(URL_BASE_TMDB + 'movie', params = payload)
+		if r.status_code == 200:
+			js = r.json()
+			titulo = js['results'][0]['title']
 		return render_template('busqueda.html', titulo = titulo)
 
 app.run('0.0.0.0', 8080, debug = True)
