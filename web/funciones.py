ccf178cda9e1704c271449aa47d068a7a5f1b65f
Álvaro Beleño <alvarobrod@outlook.es>
2018-05-09 22:32:11 +0200

    Modificado funciones.py

--- web/funciones.py
+++ web/funciones.py
@@ -1,5 +1,2 @@
 def formathora(hora):
-	dia = hora.split('-')[2]
-	mes = hora.split('-')[1]
-	año = hora.split('-')[0]
-	return '{}/{}/{}'.format(dia, mes, año)
+	return '{}/{}/{}'.format(hora.split('-')[2], hora.split('-')[1], hora.split('-')[0])
