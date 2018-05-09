9a1a51d0c5516b9c1a29fbaaa1aa06558a66d9f7
Álvaro Beleño <alvarobrod@outlook.es>
2018-05-10 00:00:00 +0200

    Modificados app.py y busqueda.html

--- web/app.py
+++ web/app.py
@@ -25,7 +25,7 @@ def busqueda():
 			r = requests.get(URL_BASE_TMDB + 'movie', params = payload)
 			if r.status_code == 200:
 				js = r.json()
-				dic_res = {'titulo': js['results'][0]['title'], 'fecha': funciones.formathora(js['results'][0]['release_date']), 'generos': funciones.genero(js['results'][0]['genre_ids']), 'sinopsis': js['results'][0]['overview']}
+				dic_res = {'titulo': js['results'][0]['title'], 'fecha': funciones.formathora(js['results'][0]['release_date']), 'generos': funciones.genero(js['results'][0]['genre_ids']), 'sinopsis': js['results'][0]['overview'], 'rating': js['results'][0]['vote_average']}
 			return render_template('busqueda.html', datos = dic_res, error = None)
 		else:
 			error = 'Debes introducir un título en el cuadro de búsqueda'
