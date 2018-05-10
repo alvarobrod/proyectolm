519dddd51881b07449720c75d6dc32c268655cb2
Álvaro Beleño <alvarobrod@outlook.es>
2018-05-10 16:47:36 +0200

    Modificaciones

--- web/app.py
+++ web/app.py
@@ -14,10 +14,10 @@ tmdb_key = os.environ['tmdb_key']
 def inicio():
 	return render_template('index.html')
 
-@app.route('/busqueda2', methods = ['GET', 'POST'])
-def busqueda2():
+@app.route('/busqueda', methods = ['GET', 'POST'])
+def busqueda():
 	if request.method == 'GET':
-		return render_template('busqueda2.html', error = None)
+		return render_template('busqueda.html', error = None)
 	else:
 		titulo_form = request.form['titulo']
 		if titulo_form != '':
@@ -29,7 +29,7 @@ def busqueda2():
 					lista = []
 					for i in js['results']:
 						lista.append({'titulo': i['title'], 'id': i['id']})
-					return render_template('busqueda2.html', datos = lista, error = None)
+					return render_template('busqueda.html', datos = lista, error = None)
 			else:
 				payload = {'api_key': tmdb_key, 'language': 'es-ES', 'query': titulo_form, 'page': '1'}
 				r = requests.get(URL_BASE_TMDB + 'tv', params = payload)
@@ -38,26 +38,10 @@ def busqueda2():
 					lista = []
 					for i in js['results']:
 						lista.append({'titulo': i['name'], 'id': i['id']})
-					return render_template('busqueda2.html', datos = lista, error = None)
+					return render_template('busqueda.html', datos = lista, error = None)
 		else:
 			error = '  Debes introducir un título en el cuadro de búsqueda'
-			return render_template('busqueda2.html', error = error)
+			return render_template('busqueda.html', error = error)
 
-@app.route('/busqueda', methods = ['GET', 'POST'])
-def busqueda():
-	if request.method == 'GET':
-		return render_template('busqueda.html', datos = None, error = None)
-	else:
-		titulo_form = request.form['titulo']
-		if titulo_form != '':
-			payload = {'api_key': tmdb_key, 'language': 'es-ES', 'query': titulo_form}
-			r = requests.get(URL_BASE_TMDB + 'movie', params = payload)
-			if r.status_code == 200:
-				js = r.json()
-				dic_res = {'titulo': js['results'][0]['title'], 'fecha': funciones.formatfecha(js['results'][0]['release_date']), 'generos': funciones.genero(js['results'][0]['genre_ids']), 'sinopsis': js['results'][0]['overview'], 'rating': js['results'][0]['vote_average'], 'votos': js['results'][0]['vote_count']}
-			return render_template('busqueda.html', datos = dic_res, error = None)
-		else:
-			error = '  Debes introducir un título en el cuadro de búsqueda'
-			return render_template('busqueda.html', datos = None, error = error)
 
 app.run('0.0.0.0', 8080, debug = True)
