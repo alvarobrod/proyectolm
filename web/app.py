9edd71e20011666604132a794a2c58a3f8809db1
Álvaro Beleño <alvarobrod@outlook.es>
2018-05-10 17:33:11 +0200

    Modificados app.py y resultado.html

--- web/app.py
+++ web/app.py
@@ -6,7 +6,8 @@ import funciones
 
 app = Flask(__name__)
 
-URL_BASE_TMDB = 'https://api.themoviedb.org/3/search/'
+URL_BASE_TMDB = 'https://api.themoviedb.org/3/'
+language = 'es-ES'
 
 tmdb_key = os.environ['tmdb_key']
 
@@ -22,8 +23,8 @@ def busqueda():
 		titulo_form = request.form['titulo']
 		if titulo_form != '':
 			if request.form['tipo'] == 'pelis':
-				payload = {'api_key': tmdb_key, 'language': 'es-ES', 'query': titulo_form, 'page': '1'}
-				r = requests.get(URL_BASE_TMDB + 'movie', params = payload)
+				payload = {'api_key': tmdb_key, 'language': language, 'query': titulo_form, 'page': '1'}
+				r = requests.get(URL_BASE_TMDB + 'search/movie', params = payload)
 				if r.status_code == 200:
 					js = r.json()
 					lista = []
@@ -31,8 +32,8 @@ def busqueda():
 						lista.append({'titulo': i['title'], 'id': i['id']})
 					return render_template('busqueda.html', datos = lista, error = None, tipo = request.form['tipo'])
 			else:
-				payload = {'api_key': tmdb_key, 'language': 'es-ES', 'query': titulo_form, 'page': '1'}
-				r = requests.get(URL_BASE_TMDB + 'tv', params = payload)
+				payload = {'api_key': tmdb_key, 'language': language, 'query': titulo_form, 'page': '1'}
+				r = requests.get(URL_BASE_TMDB + 'search/tv', params = payload)
 				if r.status_code == 200:
 					js = r.json()
 					lista = []
@@ -45,7 +46,19 @@ def busqueda():
 
 @app.route('/busqueda/<tipo>/<code>')
 def resultado(tipo, code):
-	return render_template('resultado.html', tipo = tipo, code = code)
-
+	if tipo == 'pelis':
+		payload = {'api_key': tmdb_key, 'language': language}
+		r = requests.get(URL_BASE_TMDB + 'movie/' + code, params = payload)
+		if r.status_code == 200:
+			js = r.json()
+			titulo = js['title']
+		return render_template('resultado.html', titulo = titulo)
+	else:
+		payload = {'api_key': tmdb_key, 'language': language, 'movie_id': code}
+		r = requests.get(URL_BASE_TMDB + 'movie/' + code, params = payload)
+		if r.status_code == 200:
+			js = r.json()
+			titulo = js['title']
+		return render_template('resultado.html', titulo = titulo)
 
 app.run('0.0.0.0', 8080, debug = True)
