86c897ec7b5bec0ec6cece6710c4302ad85093a6
Álvaro Beleño <alvarobrod@outlook.es>
2018-05-11 12:01:49 +0200

    Modificado app.pu

--- web/app.py
+++ web/app.py
@@ -7,6 +7,7 @@ import funciones
 app = Flask(__name__)
 
 URL_BASE_TMDB = 'https://api.themoviedb.org/3/'
+URL_BASE_IMAGE = 'https://image.tmdb.org/t/p/w300'
 language = 'es-ES'
 
 tmdb_key = os.environ['tmdb_key']
@@ -21,7 +22,7 @@ def busqueda():
 		return render_template('busqueda.html', error = None)
 	else:
 		titulo_form = request.form['titulo']
-		if titulo_form != '':
+		if titulo_form != '' and :
 			if request.form['tipo'] == 'pelis':
 				payload = {'api_key': tmdb_key, 'language': language, 'query': titulo_form, 'page': '1'}
 				r = requests.get(URL_BASE_TMDB + 'search/movie', params = payload)
@@ -41,7 +42,7 @@ def busqueda():
 						lista.append({'titulo': i['name'], 'id': i['id']})
 					return render_template('busqueda.html', datos = lista, error = None, tipo = request.form['tipo'])
 		else:
-			error = 'Debes introducir un título en el cuadro de búsqueda.'
+			error = 'Por favor, introduce un texto en el cuadro de búsqueda y selecciona películas o series.'
 			return render_template('busqueda.html', error = error)
 
 @app.route('/busqueda/<tipo>/<code>')
@@ -51,7 +52,7 @@ def resultado(tipo, code):
 		r = requests.get(URL_BASE_TMDB + 'movie/' + code, params = payload)
 		if r.status_code == 200:
 			js = r.json()
-			dic_res = {'titulo': js['title'], 'año': funciones.getaño(js['release_date']), 'rating': js['vote_average'], 'votos': js['vote_count'], 'sinopsis': js['overview'], 'generos': js['genres']}
+			dic_res = {'titulo': js['title'], 'año': funciones.getaño(js['release_date']), 'rating': js['vote_average'], 'votos': js['vote_count'], 'sinopsis': js['overview'], 'generos': js['genres'], 'poster': js['poster_path']}
 		return render_template('resultado.html', datos = dic_res)
 	else:
 		payload = {'api_key': tmdb_key, 'language': language}
@@ -59,7 +60,7 @@ def resultado(tipo, code):
 		if r.status_code == 200:
 			js = r.json()
 			titulo = js['name']
-			dic_res = {'titulo': js['name'], 'año': funciones.getaño(js['first_air_date']), 'rating': js['vote_average'], 'votos': js['vote_count'], 'sinopsis': js['overview']}
+			dic_res = {'titulo': js['name'], 'año': funciones.getaño(js['first_air_date']), 'rating': js['vote_average'], 'votos': js['vote_count'], 'sinopsis': js['overview'], 'generos': js['genres'], 'poster': js['poster_path']}
 		return render_template('resultado.html', datos = dic_res)
 
 app.run('0.0.0.0', 8080, debug = True)
