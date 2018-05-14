f015718df785a15ab4da046fecac527acf1ad640
Álvaro Beleño <alvarobrod@outlook.es>
2018-05-14 08:48:15 +0200

    Modificado app.py

--- web/app.py
+++ web/app.py
@@ -52,6 +52,8 @@ def resultado(tipo, code):
 		if r.status_code == 200:
 			js = r.json()
 			dic_res = {'titulo': js['title'], 'año': funciones.getaño(js['release_date']), 'rating': js['vote_average'], 'votos': js['vote_count'], 'sinopsis': js['overview'], 'generos': funciones.generos(js['genres']), 'poster': js['poster_path']}
+			payload2 = {'api_key': tmdb_key}
+			r2 = requests.get(URL_BASE_TMDB + 'movie/' + '{}/credits'.format(code), params = payload2)
 		return render_template('resultado.html', datos = dic_res)
 	else:
 		payload = {'api_key': tmdb_key, 'language': language}
@@ -60,6 +62,8 @@ def resultado(tipo, code):
 			js = r.json()
 			titulo = js['name']
 			dic_res = {'titulo': js['name'], 'año': funciones.getaño(js['first_air_date']), 'rating': js['vote_average'], 'votos': js['vote_count'], 'sinopsis': js['overview'], 'generos': funciones.generos(js['genres']), 'poster': js['poster_path']}
+			payload2 = {'api_key': tmdb_key}
+			r2 = requests.get(URL_BASE_TMDB + 'tv/' + '{}/credits'.format(code), params = payload2)
 		return render_template('resultado.html', datos = dic_res)
 
 app.run('0.0.0.0', 8080, debug = True)
